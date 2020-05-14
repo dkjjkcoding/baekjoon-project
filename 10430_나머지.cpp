585edc4ab7f61c5e1884a8d3595061c783875f0c
dkjjkcoding <64299694+dkjjkcoding@users.noreply.github.com>
2020-05-15 02:17:40 +0900

    (a+*b)%c==((a%c)+*(b%c))%c

---
+++ 10430_나머지.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+
+	cin >> a >> b >> c;
+	cout << (a + b) % c << endl;
+	cout << ((a % c) + (b % c)) % c << endl;
+	cout << (a * b) % c << endl;
+	cout << ((a % c) * (b % c)) % c << endl;
+
+	return 0;
+}

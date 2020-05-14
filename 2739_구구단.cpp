f56538c66255ea110eb47fcce6eeefb555c32913
dkjjkcoding <64299694+dkjjkcoding@users.noreply.github.com>
2020-05-15 01:56:19 +0900

    .

---
+++ 2739_구구단.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	for (int i = 1;i < 10;i++)
+		printf("%d * %d = %d\n", n, i, n * i);
+
+	return 0;
+}

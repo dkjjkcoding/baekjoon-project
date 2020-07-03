f38b8ba51c4f819e0f7ffbffbb08f676cd250787
dkjjkcoding <64299694+dkjjkcoding@users.noreply.github.com>
2020-07-03 22:52:38 +0900

    <algorithm> next_permutation()

---
+++ 10972_다음순열.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a[10000];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0;i < n;i++)
+		cin >> a[i];
+
+	if (next_permutation(a, a + n))
+		for (int i = 0;i < n;i++)
+			cout << a[i] << ' ';
+	else
+		cout << -1;
+
+	return 0;
+}

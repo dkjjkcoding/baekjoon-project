b120eac7727bbb92d461201ca5f66ab510a47706
최소원 <64299694+dkjjkcoding@users.noreply.github.com>
2020-07-09 22:25:00 +0900

    n(중복순열)m -> nPm(dfs구현)에서 use[MAX] 사용X
    중복가능하므로 사용여부 확인할 필요 없음 => use[] 사용X

---
+++ 15651_n과m(3).cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+
+#define MAX 7
+#pragma warning(disable:4996)
+using namespace std;
+
+int n,m;
+int a[]={1,2,3,4,5,6,7,8};
+
+void dfs(string out,int depth){
+  if(depth==m){
+    for(char c: out)
+      cout<<c<<' ';
+    cout<<'\n';
+    return;
+  }
+  for(int i=0;i<n;i++){
+    out+=to_string(a[i]);
+    dfs(out,depth+1);
+    out=out.substr(0,out.length()-1);
+  }
+}
+
+void solution(){
+  cin>>n>>m;
+  dfs("",0);
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  solution();
+  
+  return 0;
+}

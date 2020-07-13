c954654201cee07c6d4b5e2dd5b15cbb58200765
최소원 <64299694+dkjjkcoding@users.noreply.github.com>
2020-07-13 22:46:00 +0900

    next_permutation( ) + use[ ] -> nCm

---
+++ 15650_N과M(2).cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+#define MAX 8
+#pragma warning(disable:4996)
+using namespace std;
+
+int n,m;
+int a[]={1,2,3,4,5,6,7,8};
+int use[MAX]; // y(0), n(1)
+vector<int> v;
+
+void solution(){
+  cin>>n>>m;
+  for(int i=n-1;i>m-1;i--)
+    use[i]=1;
+  
+  do{
+    v.clear();
+    for(int i=0;i<n;i++)
+      if(use[i]==0) // use(0), no-use(1)
+        v.push_back(a[i]);
+    for(int i=0;i<v.size();i++)
+      cout<<v[i]<<' ';
+    cout<<'\n';
+  }while(next_permutation(use,use+n));
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  solution();
+  
+  return 0;
+}

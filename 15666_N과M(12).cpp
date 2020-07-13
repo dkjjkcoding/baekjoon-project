d1bc77a2e20718d62eb32f5bcdc10be4b518d85c
최소원 <64299694+dkjjkcoding@users.noreply.github.com>
2020-07-14 00:00:24 +0900

    .

---
+++ 15666_N과M(12).cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+
+#define MAx 7
+#pragma warning(disable:4996)
+using namespace std;
+
+int n,m,temp;
+vector<int> v;
+map<int,int> cnt;
+int point[MAX];
+
+void dfs(int idx,int jdx,int depth){
+  if(depth==m){
+    for(int i=0;i<m;i++)
+      cout<<v[point[i]]<<' ';
+    cout<<'\n';
+    return;
+  }
+  for(int i=jdx;i<v.size();i++){
+    if(cnt[v[i]]){
+      point[idx]=i;
+      dfs(idx+1,i,depth+1);
+    }
+  }
+}
+
+void solution(){
+  cin>>n>>m;
+  for(int i=0;i<n;i++){
+    cin>>temp;
+    if(!cnt.count(temp)){
+      cnt[temp]=1;
+      v.push_back(temp);
+    }
+    else
+      cnt[temp]++;
+  }
+  sort(v.begin(),v.end());
+  dfs(0,0,0);
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  solution();
+  
+  return 0;
+}

31dfa5601905991af8bd24f54ccbe7ae443b5102
최소원 <64299694+dkjjkcoding@users.noreply.github.com>
2020-07-22 23:23:08 +0900

    dp는 대부분 bottom-up. d[1]부터 시작하여 d[n] 도달.

---
+++ 11052_카드구매하기.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+
+#define MAX 1000+1
+#pragma warning(disable:4996)
+using namespcae std;
+
+int n;
+int p[MAX],dp[MAX];
+
+void solution(){
+  cin>>n;
+  for(int i=1;i<=n;i++)
+    cin>>p[i];
+  
+  for(int i=1;i<=n;i++){
+    dp[i]=p[i];
+    for(int j=1;j<i;j++){
+      dp[i]=max(dp[i],dp[i-j]+p[j]) // 마지막에 몇 개 팩을 사느냐를 생각.
+    }
+  }
+  
+  cout<<dp[n];
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  solution();
+  
+  return 0;
+}

a0d8fd4e5d134ca4956ca709c456ce6b86f00735
최소원 <64299694+dkjjkcoding@users.noreply.github.com>
2020-08-04 22:01:53 +0900

    dp.

---
+++ 11055_가장큰증가부분수열.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+
+#define MAX 1000
+#pragma warning(disable:4996)
+using namespace std;
+
+int n;
+int inp[MAX];
+int dp[MAX];
+
+void solution(){
+  cin>>n;
+  for(int i=0;i<n;i+) cin>>inp[i];
+  
+  dp[0]=inp[0];
+  for(int i=1;i<n;i++){
+    for(int j=i-1;j>=0;j--){
+      int temp=dp[j]+inp[i];
+      if(inp[j]<inp[i]&&temp>dp[i])
+        dp[i]=temp;
+    }
+    if(dp[i]==0)
+      dp[i]=inp[i];
+  }
+  
+  cout<<*max_element(dp,dp+n);
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  solution();
+  
+  return 0;
+}

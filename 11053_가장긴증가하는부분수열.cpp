956c006e9126630da575674410c810d846ce51df
최소원 <64299694+dkjjkcoding@users.noreply.github.com>
2020-08-04 21:48:19 +0900

    dp.

---
+++ 11053_가장긴증가하는부분수열.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+
+#define MAX 1000
+#pragma warning(disable:4996)
+using namespace std;
+
+int n;
+int inp[MAX];
+int dp[MAX];
+
+void solution(){
+  cin>>n;
+  for(int i=0;i<n;i++)
+    cin>>inp[i];
+    
+  dp[0]=1;
+  for(int i=1;i<n;i++){
+    for(int j=i-1;j>=0;j--){
+      if(inp[j]<inp[i]&&dp[j]+1>dp[i])
+        dp[i]=dp[j]+1;
+    }
+    if(dp[i]==0)
+      dp[i]=1;
+  }
+  
+  cout<<*max_element(dp,dp+n);
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  solution();
+  
+  return 0;
+}
